e56a415bc9149e6165ce41100eb4a4593d7aab9c
Matty Evans <mattevansnz@gmail.com>
2025-01-08 10:46:03 +1000

    feat: Update event data handling for chain reorg and contribution proof

--- internal/events/v1/chain_reorg.go
+++ internal/events/v1/chain_reorg.go
@@ -5,9 +5,11 @@ import (
 
 	eth2v1 "github.com/attestantio/go-eth2-client/api/v1"
 	"github.com/ethpandaops/contributoor/internal/events"
+	xatuethv1 "github.com/ethpandaops/xatu/pkg/proto/eth/v1"
 	"github.com/ethpandaops/xatu/pkg/proto/xatu"
 	"github.com/google/uuid"
 	"google.golang.org/protobuf/types/known/timestamppb"
+	"google.golang.org/protobuf/types/known/wrapperspb"
 )
 
 // ChainReorgEvent represents a beacon chain reorganization event.
@@ -44,17 +46,37 @@ func (e *ChainReorgEvent) Decorated() *xatu.DecoratedEvent {
 			DateTime: timestamppb.New(e.recvTime),
 			Id:       uuid.New().String(),
 		},
-		Data: &xatu.DecoratedEvent_EthV1EventsChainReorgV2{},
+		Data: &xatu.DecoratedEvent_EthV1EventsChainReorgV2{
+			EthV1EventsChainReorgV2: xatuethv1.NewReorgEventV2FromGoEth2ClientEvent(e.data),
+		},
 	}
 
 	if e.beacon == nil {
 		return decorated
 	}
 
-	//TODO(@matty): Populate additional data.
-	extra := &xatu.ClientMeta_AdditionalEthV1EventsChainReorgV2Data{}
+	var (
+		slot  = e.beacon.GetSlot(uint64(e.data.Slot))
+		epoch = e.beacon.GetEpochFromSlot(uint64(e.data.Slot))
+	)
+
 	decorated.Meta.Client.AdditionalData = &xatu.ClientMeta_EthV1EventsChainReorgV2{
-		EthV1EventsChainReorgV2: extra,
+		EthV1EventsChainReorgV2: &xatu.ClientMeta_AdditionalEthV1EventsChainReorgV2Data{
+			Slot: &xatu.SlotV2{
+				Number:        &wrapperspb.UInt64Value{Value: uint64(e.data.Slot)},
+				StartDateTime: timestamppb.New(slot.TimeWindow().Start()),
+			},
+			Epoch: &xatu.EpochV2{
+				Number:        &wrapperspb.UInt64Value{Value: epoch.Number()},
+				StartDateTime: timestamppb.New(epoch.TimeWindow().Start()),
+			},
+			Propagation: &xatu.PropagationV2{
+				SlotStartDiff: &wrapperspb.UInt64Value{
+					//nolint:gosec // not concerned in reality
+					Value: uint64(e.recvTime.Sub(slot.TimeWindow().Start()).Milliseconds()),
+				},
+			},
+		},
 	}
 
 	return decorated
--- internal/events/v1/contribution_and_proof.go
+++ internal/events/v1/contribution_and_proof.go
@@ -5,9 +5,11 @@ import (
 
 	"github.com/attestantio/go-eth2-client/spec/altair"
 	"github.com/ethpandaops/contributoor/internal/events"
+	xatuethv1 "github.com/ethpandaops/xatu/pkg/proto/eth/v1"
 	"github.com/ethpandaops/xatu/pkg/proto/xatu"
 	"github.com/google/uuid"
 	"google.golang.org/protobuf/types/known/timestamppb"
+	"google.golang.org/protobuf/types/known/wrapperspb"
 )
 
 // ContributionAndProofEvent represents a sync committee contribution and proof event.
@@ -44,15 +46,52 @@ func (e *ContributionAndProofEvent) Decorated() *xatu.DecoratedEvent {
 			DateTime: timestamppb.New(e.recvTime),
 			Id:       uuid.New().String(),
 		},
-		Data: &xatu.DecoratedEvent_EthV1EventsContributionAndProofV2{},
+		Data: &xatu.DecoratedEvent_EthV1EventsContributionAndProofV2{
+			EthV1EventsContributionAndProofV2: &xatuethv1.EventContributionAndProofV2{
+				Signature: xatuethv1.TrimmedString(xatuethv1.BLSSignatureToString(&e.data.Signature)),
+				Message: &xatuethv1.ContributionAndProofV2{
+					AggregatorIndex: &wrapperspb.UInt64Value{Value: uint64(e.data.Message.AggregatorIndex)},
+					SelectionProof:  xatuethv1.TrimmedString(xatuethv1.BLSSignatureToString(&e.data.Message.SelectionProof)),
+					Contribution: &xatuethv1.SyncCommitteeContributionV2{
+						Slot:              &wrapperspb.UInt64Value{Value: uint64(e.data.Message.Contribution.Slot)},
+						SubcommitteeIndex: &wrapperspb.UInt64Value{Value: e.data.Message.Contribution.SubcommitteeIndex},
+						AggregationBits:   xatuethv1.BytesToString(e.data.Message.Contribution.AggregationBits.Bytes()),
+						Signature:         xatuethv1.TrimmedString(xatuethv1.BLSSignatureToString(&e.data.Message.Contribution.Signature)),
+						BeaconBlockRoot:   xatuethv1.RootAsString(e.data.Message.Contribution.BeaconBlockRoot),
+					},
+				},
+			},
+		},
 	}
 
 	if e.beacon == nil {
 		return decorated
 	}
 
-	//TODO(@matty): Populate additional data.
-	extra := &xatu.ClientMeta_AdditionalEthV1EventsContributionAndProofV2Data{}
+	var (
+		slot  = e.beacon.GetSlot(uint64(e.data.Message.Contribution.Slot))
+		epoch = e.beacon.GetEpochFromSlot(uint64(e.data.Message.Contribution.Slot))
+	)
+
+	extra := &xatu.ClientMeta_AdditionalEthV1EventsContributionAndProofV2Data{
+		Contribution: &xatu.ClientMeta_AdditionalEthV1EventsContributionAndProofContributionV2Data{
+			Slot: &xatu.SlotV2{
+				Number:        &wrapperspb.UInt64Value{Value: slot.Number()},
+				StartDateTime: timestamppb.New(slot.TimeWindow().Start()),
+			},
+			Epoch: &xatu.EpochV2{
+				Number:        &wrapperspb.UInt64Value{Value: epoch.Number()},
+				StartDateTime: timestamppb.New(epoch.TimeWindow().Start()),
+			},
+			Propagation: &xatu.PropagationV2{
+				SlotStartDiff: &wrapperspb.UInt64Value{
+					//nolint:gosec // not concerned in reality
+					Value: uint64(e.recvTime.Sub(slot.TimeWindow().Start()).Milliseconds()),
+				},
+			},
+		},
+	}
+
 	decorated.Meta.Client.AdditionalData = &xatu.ClientMeta_EthV1EventsContributionAndProofV2{
 		EthV1EventsContributionAndProofV2: extra,
 	}
